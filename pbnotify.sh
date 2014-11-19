59f9049799f3329f95c61d808c98e2007b461ca6
lemed <mehdi.dettinger@gmail.com>
2014-11-19 19:47:42 +0100

    MaJ README et appel du SHELL pour exécution sur NAS

--- pbnotify.sh
+++ pbnotify.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-apikey="your_token"
+apikey="c2H745CaIdFZG4WpY1fx43L39rPANdtu"
 title="$1"
 message="$2"
 curl -s "https://api.pushbullet.com/v2/pushes" -u $apikey: -d type=note -d title="$title" --data-urlencode body="$message" -X POST
