74ceb7ef4a4fdd7aca9199e4316ced49658848ee
lemed <mehdi.dettinger@gmail.com>
2014-11-30 22:46:28 +0100

    anonymize

--- pbnotify.sh
+++ pbnotify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apikey="c2H745CaIdFZG4WpY1fx43L39rPANdtu"
+apikey="YOUR_API_KEY_HERE"
 title="$1"
 message="$2"
 curl -s "https://api.pushbullet.com/v2/pushes" -u $apikey: -d type=note -d title="$title" --data-urlencode body="$message" -X POST
